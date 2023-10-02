35812ab4799d3b5de98d3c07b241095dfba28a0a
Kristaps <esmitskristaps@inbox.lv>
2023-10-02 22:33:12 +0300

    sviest

---
+++ Frontend/src/App.vue
@@ -0,0 +1,40 @@
+<script setup>
+import {RouterLink, RouterView} from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <RouterLink to="/" class="navbar-brand">Home</RouterLink>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                  data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                  aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <RouterLink to="/" class="nav-link">Home</RouterLink>
+              </li>
+              <li class="nav-item">
+                <RouterLink to="/about" class="nav-link">About</RouterLink>
+              </li>
+              <li class="nav-item">
+                <RouterLink to="/login" class="nav-link">Login</RouterLink>
+              </li>
+            </ul>
+            <form class="d-flex" role="search">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+          </div>
+        </div>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView/>
+</template>
+
---
+++ Frontend/src/views/HomeView.vue
@@ -0,0 +1,5 @@
+<template>
+  <main>
+    <h1>Welcome to aaaaas</h1>
+  </main>
+</template>
