6176c7248aa2249bd734ba5422e547d318388fbd
Kristaps <esmitskristaps@inbox.lv>
2023-10-17 21:33:05 +0300

    laravel crud test

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,18 +2,15 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+use App\Http\Controllers\Api\Testcontroller;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('test', [Testcontroller::class, 'index']);
+Route::post('test', [Testcontroller::class, 'store']);
+Route::get('test/{id}', [Testcontroller::class, 'show']);
+Route::get('test/{id}/edit', [Testcontroller::class, 'edit']);
+Route::put('test/{id}/edit', [Testcontroller::class, 'update']);
+Route::delete('test/{id}/delete', [Testcontroller::class, 'destroy']);
