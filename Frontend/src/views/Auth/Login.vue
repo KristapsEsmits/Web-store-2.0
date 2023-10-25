8f941d97d1e24a18ba36d58ec105418c0daf6fd8
Kristaps <esmitskristaps@inbox.lv>
2023-10-25 21:27:02 +0300

    migration

--- Frontend/src/views/Auth/Login.vue
+++ Frontend/src/views/Auth/Login.vue
@@ -8,14 +8,14 @@
                 <form>
                     <div class="form-group">
                         <label for="exampleInputEmail1">Email address</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                        <input type="email" class="form-control" placeholder="Enter email">
                     </div>
                     <div class="form-group">
                         <label for="exampleInputPassword1">Password</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                        <input type="password" class="form-control" placeholder="Password">
                     </div>
                     <div class="form-group form-check">
-                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                        <input type="checkbox" class="form-check-input">
                         <label class="form-check-label" for="exampleCheck1">Check me out</label>
                     </div>
                     <button type="submit" class="btn btn-primary">Submit</button>
--- Frontend/src/views/Auth/Register.vue
+++ Frontend/src/views/Auth/Register.vue
@@ -8,35 +8,31 @@
                 <form>
                     <div class="form-group">
                         <label for="exampleInputEmail1">Username</label>
-                        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Username">
+                        <input type="text" class="form-control" aria-describedby="emailHelp" placeholder="Username">
                     </div>
                     <div class="form-group">
                         <label for="exampleInputPassword1">Password</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                        <input type="password" class="form-control" placeholder="Password">
                     </div>
                     <div class="form-group">
                         <label for="exampleInputPassword1">Repeat password</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Repeat Password">
+                        <input type="password" class="form-control" placeholder="Repeat Password">
                     </div>
                     <div class="form-group">
                         <label for="exampleInputEmail1">Name</label>
-                        <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Name">
+                        <input type="text" class="form-control" placeholder="Name">
                     </div>
                     <div class="form-group">
                         <label for="exampleInputEmail1">Surname</label>
-                        <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Surname">
+                        <input type="text" class="form-control" placeholder="Surname">
                     </div>
                     <div class="form-group">
                         <label for="exampleInputEmail1">Number</label>
-                        <input type="number" class="form-control" id="exampleInputEmail1" placeholder="Number">
+                        <input type="number" class="form-control" placeholder="Number">
                     </div>
                     <div class="form-group">
                         <label for="exampleInputEmail1">Email address</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-                    </div>
-                    <div class="form-group form-check">
-                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                        <input type="email" class="form-control" placeholder="Enter email">
                     </div>
                     <button type="submit" class="btn btn-primary">Submit</button>
                     </form>
