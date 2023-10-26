1d0c27c5d3840a45bd0b238369007391d1f568f8
Kristaps <esmitskristaps@inbox.lv>
2023-10-26 23:01:45 +0300

    API fix

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\Testcontroller;
+use App\Http\Controllers\Api\Authcontroller;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,14 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//Test API
+Route::get('test', [Testcontroller::class, 'index']);
+Route::post('test', [Testcontroller::class, 'store']);
+Route::get('test/{id}', [Testcontroller::class, 'show']);
+Route::get('test/{id}/edit', [Testcontroller::class, 'edit']);
+Route::put('test/{id}/edit', [Testcontroller::class, 'update']);
+Route::delete('test/{id}/delete', [Testcontroller::class, 'destroy']);
+
+//Auth API
+Route::post('register', [Authcontroller::class, 'register']);
