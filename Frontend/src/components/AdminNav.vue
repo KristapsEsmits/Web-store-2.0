31ecf3e6bd69dddf730006a23e6a4a4691bfc1aa
Kristaps <esmitskristaps@inbox.lv>
2023-12-21 02:33:32 +0200

    bug fix

--- Frontend/src/components/AdminNav.vue
+++ Frontend/src/components/AdminNav.vue
@@ -1,20 +1,19 @@
 <template>
   <div :class="{ 'nav': true, 'active': isSidebarOpen }">
-    <div class="links">
-      <router-link to="/admin">
-        <button class="btn">Home</button>
-      </router-link>
-      <router-link to="/test">
-        <button class="btn">Test</button>
-      </router-link>
-      <router-link to="/admin/brands">
-        <button class="btn">Brands</button>
-      </router-link>
-      <router-link to="/admin/items">
-        <button class="btn">Items</button>
-      </router-link>
-    </div>
-    
+  <div class="links">
+    <router-link to="/admin">
+      <button class="btn">Home</button>
+    </router-link>
+    <router-link to="/test">
+      <button class="btn">Test</button>
+    </router-link>
+    <router-link to="/admin/brands">
+      <button class="btn">Brands</button>
+    </router-link>
+    <router-link to="/admin/items">
+      <button class="btn">Items</button>
+    </router-link>
+  </div>
     <button @click="toggleSidebar" class="btn toggle-btn">Toggle Sidebar</button>
   </div>
 </template>
@@ -22,7 +21,7 @@
 <script setup>
 import { ref } from 'vue';
 
-const isSidebarOpen = ref(falsegf);
+const isSidebarOpen = ref(false);
 
 const toggleSidebar = () => {
   isSidebarOpen.value = !isSidebarOpen.value;
@@ -30,18 +29,17 @@ const toggleSidebar = () => {
 </script>
 
 <style scoped>
-/* Add your existing styles */
 .nav {
   position: fixed;
   top: 59.7px;
   left: 0;
   height: 100%;
-  background-color: #f8f9fa; /* Add your preferred background color */
-  padding-top: 20px; /* Add padding as needed */
+  background-color: #f8f9fa; 
+  padding-top: 20px; 
 }
 
 .nav.active {
-  margin-left: -250px; /* Adjust the width of your sidebar */
+  margin-left: -250px;
 }
 
 .btn {
@@ -52,7 +50,6 @@ const toggleSidebar = () => {
 }
 
 .toggle-btn {
-  /* Style your toggle button as needed */
   position: fixed;
   bottom: 20px;
   left: 10px;
